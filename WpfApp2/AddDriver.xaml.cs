dc51273591b83ab5808f815dbcb0e0f1c57fd532
Aryan Sharma <aryansharma.bkn@gmail.com>
2023-11-29 13:25:36 +0530

    Added add_driver

---
+++ WpfApp2/AddDriver.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp2
+{
+    public partial class AddDriver : Window
+    {
+        public AddDriver()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+
+    }
+}
